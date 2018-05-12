b9a6e57d50ace650f4f8e385d48fac0d7ffbff6e
Budi Oktaviyan Suryanto <budioktaviyans@gmail.com>
2018-05-13 01:20:32 +0700

    Initial sample

---
+++ sample/src/main/java/id/kotlin/tooltip/sample/FirstActivity.kt
@@ -0,0 +1,12 @@
+package id.kotlin.tooltip.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class FirstActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+    }
+}
